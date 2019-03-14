8ebb6ba5ea2df6ecc4d59624f3b84e80ecb6f87a
Syed <Irfan.Syed@elektrobit.com>
2019-03-14 13:12:44 +0530

    test coverage

--- tests/plugin.test.js
+++ tests/plugin.test.js
@@ -1,17 +1,55 @@
-import pluginTester from "babel-plugin-tester";
-import babelParser from "../plugins/plugin";
+const babelParser = require("../plugins/plugin");
+const fs = require("fs");
+const astJson = require("./astExtract.json");
 
-// pluginTester({
-//   plugin: babelParser,
-//   snapshot: true,
-//   tests: [
-//     {
-//       code: ""
-//     }
-//   ]
-// });
+describe("babel plugin tests", () => {
+  it("should check for label and uuid ", () => {
+    const val = babelParser();
+    const inputNode = astJson[0];
+    const nodeAttributes = inputNode.attributes;
+    nodeAttributes.forEach(attribute => {
+      if (attribute.name.name === "uuid") {
+        expect(attribute.value.value.length).toBeGreaterThan(0);
+        expect(typeof attribute.value.value).toBe("string");
+      }
+    });
+    val.visitor.JSXOpeningElement({ node: inputNode });
+    expect(inputNode.name.name).toEqual("Label");
+  });
 
-test("should get passed", () => {
-  expect(1).toEqual(1);
-  babelParser();
+  it("should check no label tag", () => {
+    const val = babelParser();
+    const inputNode = astJson[2];
+    val.visitor.JSXOpeningElement({ node: inputNode });
+    expect(inputNode.name.name).not.toEqual("Label");
+  });
+  it("write file test", function(done) {
+    fs.writeFile(
+      "label-tag-export.json",
+      JSON.stringify({}, null, "\t"),
+      function(err) {
+        if (err) {
+          throw err;
+        }
+        expect(3).to.equal(4); // Should fail
+        done();
+      }
+    );
+  });
+  it("should check for label but no uuid ", () => {
+    const val = babelParser();
+    const inputNode = astJson[1];
+    val.visitor.JSXOpeningElement({ node: inputNode });
+
+    expect(inputNode.name.name).toEqual("Label");
+
+    const nodeAttributes = inputNode.attributes;
+    nodeAttributes.forEach(attribute => {
+      if (attribute.name.name !== "uuid") {
+        expect(attribute.value.name).not.toBe("uuid");
+      } else {
+        expect(attribute.value.name).not.toBe("uuid");
+      }
+    });
+  });
 });
--- tests/pluginTests.tsx
+++ tests/pluginTests.tsx
@@ -5,7 +5,7 @@ export default class DemosView {
       <>
         <Label
           class="new-class"
-          descriprtion="new-desc"
+          description="new-desc"
           uuid="56bd5942-1813-43f1-b719-ab1f20ddfdca"
         />
         <Label class="new-class1" description="new-desc1" />
